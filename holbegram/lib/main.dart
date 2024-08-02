f4680006be82c7de91f080b4f3740b24daa17e39
s4iTen <ychouk102@gmail.com>
2024-08-03 01:46:34 +0200

    this is the last updates

--- holbegram/lib/main.dart
+++ holbegram/lib/main.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
 }
