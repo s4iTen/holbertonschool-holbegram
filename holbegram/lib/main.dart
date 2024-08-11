748b677f8428b73b7812fb2a59467ba7476280d5
s4iten <ychouk102@gmail.com>
2024-08-11 02:48:13 +0200

    this is the last updates

--- holbegram/lib/main.dart
+++ holbegram/lib/main.dart
@@ -1,8 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 
-Future main() async {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Holbegram',
+    );
+  }
 }
---
+++ holbegram/lib/widgets/text_field.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+class TextFieldInput extends StatelessWidget {
+  final TextEditingController controller;
+  final bool isPassword;
+  final String hintText;
+  final Widget? suffixIcon;
+  final TextInputType keyboardType;
+
+  const TextFieldInput({
+    Key? key,
+    required this.controller,
+    this.isPassword = false,
+    required this.hintText,
+    this.suffixIcon,
+    this.keyboardType = TextInputType.text,
+  }) : super(key: key);
+
+ @override
+  Widget build(BuildContext context) {
+    return TextField(
+      keyboardType: keyboardType,
+      controller: controller,
+      cursorColor: const Color.fromARGB(218, 226, 37, 24),
+      obscureText: isPassword,
+      textInputAction: TextInputAction.next,
+      decoration: InputDecoration(
+        hintText: hintText,
+        border: const OutlineInputBorder(
+          borderSide: BorderSide(color: Colors.transparent),
+        ),
+        focusedBorder: const OutlineInputBorder(
+          borderSide: BorderSide(color: Colors.transparent),
+        ),
+        enabledBorder: const OutlineInputBorder(
+          borderSide: BorderSide(color: Colors.transparent),
+        ),
+        filled: true,
+        contentPadding: const EdgeInsets.all(8),
+        suffixIcon: suffixIcon,
+      ),
+    );
+  }
+}
